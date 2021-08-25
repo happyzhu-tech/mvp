e4734a488bebc51008d2b4a94eaa41c26e8c7278
Jiaqian Zhu <blairzhu19@gmail.com>
2021-08-24 17:54:26 -0700

    add and cook buttons are diabled when ingredients haven't been added

--- client/src/components/AddIngredient/AddIngredient.jsx
+++ client/src/components/AddIngredient/AddIngredient.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './AddIngredient.module.css';
-import ingredients from './ingredients.png';
+import ingredientImg from './ingredients.png';
+import add from './add.png';
+import cook from './cook.png';
 
 const AddIngredient = () => {
+  const [ingredients, setIngredients] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const inputChangeHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className={style.rightContent}>
       <h2>What Ingredients Do You Have At Home?</h2>
-      <img src={ingredients} className={style.image}></img>
+      <img src={ingredientImg} className={style.image}></img>
       <div>
         <label htmlFor='ingredient' className={style.label}>Ingredient you have:</label>
-        <input type='text' name='ingredient' id='ingredient' className={style.inputBox}></input>
+        <input type='text' name='ingredient' id='ingredient' className={style.inputBox} value={inputValue}
+          onChange={inputChangeHandler}></input>
       </div>
-      <button className={style.button}>Add</button>
-      <button className={style.button}>Cook</button>
+      <div className={style.buttons}>
+        <div className={style.bigButton}>
+          <button className={style.button} disabled={!inputValue}>Add</button>
+          <img src={add} className={style.icon}></img>
+        </div>
+        <div className={style.bigButton}>
+          <button className={style.button} disabled={!ingredients.length}>Cook</button>
+          <img src={cook} className={style.icon}></img>
+        </div>
+      </div>
     </div>
   );
 };
